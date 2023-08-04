35365acb3093a94754798d41e69f1b8323cf4636
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-08-04 06:02:40 +0300

    [UPDATE] get channel metadata is now returnning the members and the creator

--- src/app/models/channels.model.js
+++ src/app/models/channels.model.js
@@ -6,22 +6,65 @@ class Channel {
 
       const channel = await db('channels')
          .select(
-            'id',
-            'name',
-            'description',
-            'image_url',
-            'status',
-            'type',
-            'creator',
-            'created_at'
+            'channels.id',
+            'channels.name',
+            'channels.image_url',
+            'channels.description',
+            'channels.type',
+            db.raw(
+               `json_build_object('id', creator.id, 'name', creator.name, 'image_url', creator.image_url) as creator`
+            ),
+            db.raw(
+               `json_agg(json_build_object('id', members.user_id, 'role', members.role, 'name', users.name, 'image_url', users.image_url)) as members`
+            )
          )
-         .where('id', channelId)
+         .leftJoin('users as creator', 'creator.id', 'channels.creator')
+         .leftJoin(
+            'channel_members as members',
+            'channels.id',
+            'members.channel_id'
+         )
+         .leftJoin('users', 'users.id', 'members.user_id')
+         .where('channels.id', channelId)
+         .andWhere('channels.status', 'active')
          .andWhere(function () {
-            this.where('type', 'public').orWhere(function () {
-               this.where('type', 'private').andWhere('creator', creatorId)
+            this.where('channels.type', 'public').orWhere(function () {
+               this.where('channels.type', 'private').andWhere(
+                  'channels.creator',
+                  creatorId
+               )
             })
          })
+         .groupBy('channels.id', 'creator.id')
          .first()
+
+      // const channel = await db('channels')
+      // .select(
+      //   'channels.id',
+      //   'channels.name',
+      //   'channels.description',
+      //   'channels.image_url',
+      //   'channels.status',
+      //   'channels.type',
+      //   'channels.created_at',
+      //   db.raw(`json_build_object('id', users.id, 'name', users.name, 'image_url', users.image_url) as creator`),
+      // )
+      // .from('channels')
+      // .leftJoin('users', 'users.id', 'channels.creator')
+
+      // .groupBy(
+      //   'channels.id',
+      //   'channels.name',
+      //   'channels.description',
+      //   'channels.image_url',
+      //   'channels.status',
+      //   'channels.type',
+      //   'users.id',
+      //   'users.name',
+      //   'users.image_url',
+      //   'channels.created_at'
+      // )
+      // .first();
 
       return channel
    }
