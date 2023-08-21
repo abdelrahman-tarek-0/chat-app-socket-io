fef19193d3f1bde2ae197ce2727745a97eceaf0a
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-08-21 09:03:17 +0300

    [UPDATE] channel validators is done and tested

--- src/app/validators/channels.validators.js
+++ src/app/validators/channels.validators.js
@@ -2,116 +2,106 @@ const { body, param, query } = require('express-validator')
 const { handleValidationErrors } = require('./base.validators')
 
 exports.getChannel = [
-    param('id')
-        .trim()
-        .isUUID(4)
-        .withMessage('Channel id is not valid'),
+   param('id').trim().isUUID(4).withMessage('Channel id is not valid'),
 
-    handleValidationErrors,
+   handleValidationErrors,
 ]
 
-
 exports.getAllChannels = [
-    query('order[by]')
-        .optional()
-        .trim()
-        .isIn(['id', 'name', 'type', 'members_count', 'created_at'])
-        .withMessage('Order by is not valid'),
-    
-    query('order[dir]')
-        .optional()
-        .trim()
-        .isIn(['asc', 'desc'])
-        .withMessage('Order direction is not valid'),
+   query('order[by]')
+      .optional()
+      .trim()
+      .isIn(['id', 'name', 'type', 'members_count', 'created_at'])
+      .withMessage('Order by is not valid'),
 
-    query('pagination[page]')
-        .optional()
-        .trim()
-        .isInt({ min: 1 })
-        .withMessage('Pagination page must be a number and greater than 0'),
+   query('order[dir]')
+      .optional()
+      .trim()
+      .isIn(['asc', 'desc'])
+      .withMessage('Order direction is not valid'),
 
-    query('pagination[limit]')
-        .optional()
-        .trim()
-        .isInt({ min: 1, max: 50 })
-        .withMessage('Pagination limit must be a number between 1 and 50'),
+   query('pagination[page]')
+      .optional()
+      .trim()
+      .isInt({ min: 1 })
+      .withMessage('Pagination page must be a number and greater than 0'),
 
-    query('search') 
-        .optional()
-        .trim()
-        .isLength( { max: 255 } )
-        .withMessage('Search string must be less than 255 characters'),
+   query('pagination[limit]')
+      .optional()
+      .trim()
+      .isInt({ min: 1, max: 50 })
+      .withMessage('Pagination limit must be a number between 1 and 50'),
 
-    handleValidationErrors,
+   query('search')
+      .optional()
+      .trim()
+      .isLength({ max: 255 })
+      .withMessage('Search string must be less than 255 characters'),
+
+   handleValidationErrors,
 ]
 
 exports.createChannel = [
-    body('name')
-        .trim()
-        .isLength({ min: 1, max: 255 })
-        .withMessage('Channel name must be between 1 and 255 characters'),
+   body('name')
+      .trim()
+      .isLength({ min: 1, max: 255 })
+      .withMessage('Channel name must be between 1 and 255 characters'),
 
-    body('description')
-        .trim()
-        .isLength({ min: 1, max: 1023 })
-        .withMessage('Channel description must be between 1 and 1023 characters'),
+   body('description')
+      .trim()
+      .isLength({ min: 1, max: 1023 })
+      .withMessage('Channel description must be between 1 and 1023 characters'),
 
-    body('image_url')
-        .optional()
-        .trim()
-        .isURL()
-        .withMessage('Channel image url is not valid')
-        .isLength({max: 512 })
-        .withMessage('Channel image url must be less than 512 characters'),
+   body('image_url')
+      .optional()
+      .trim()
+      .isURL()
+      .withMessage('Channel image url is not valid')
+      .isLength({ max: 512 })
+      .withMessage('Channel image url must be less than 512 characters'),
 
-    body('type')
-        .trim()
-        .isIn(['public', 'private'])
-        .withMessage('Channel type must be public or private'),
+   body('type')
+      .trim()
+      .isIn(['public', 'private'])
+      .withMessage('Channel type must be public or private'),
 
-    handleValidationErrors,
+   handleValidationErrors,
 ]
 
 exports.updateChannel = [
-    param('id')
-        .trim()
-        .isUUID(4)
-        .withMessage('Channel id is not valid'),
+   param('id').trim().isUUID(4).withMessage('Channel id is not valid'),
 
-    body('name')
-        .optional()
-        .trim() 
-        .isLength({ min: 1, max: 255 })
-        .withMessage('Channel name must be between 1 and 255 characters'),
-        
-    body('description') 
-        .optional()
-        .trim() 
-        .isLength({ min: 1, max: 1023 })    
-        .withMessage('Channel description must be between 1 and 1023 characters'),
+   body('name')
+      .optional()
+      .trim()
+      .isLength({ min: 1, max: 255 })
+      .withMessage('Channel name must be between 1 and 255 characters'),
 
-    body('image_url')
-        .optional()
-        .trim()
-        .isURL()
-        .withMessage('Channel image url is not valid')
-        .isLength({max: 512 })
-        .withMessage('Channel image url must be less than 512 characters'),
+   body('description')
+      .optional()
+      .trim()
+      .isLength({ min: 1, max: 1023 })
+      .withMessage('Channel description must be between 1 and 1023 characters'),
 
-    body('type')
-        .optional() 
-        .trim()
-        .isIn(['public', 'private'])
-        .withMessage('Channel type must be public or private'),
+   body('image_url')
+      .optional()
+      .trim()
+      .isURL()
+      .withMessage('Channel image url is not valid')
+      .isLength({ max: 512 })
+      .withMessage('Channel image url must be less than 512 characters'),
 
-    handleValidationErrors,
+   body('type')
+      .optional()
+      .trim()
+      .isIn(['public', 'private'])
+      .withMessage('Channel type must be public or private'),
+
+   handleValidationErrors,
 ]
 
 exports.deleteChannel = [
-    param('id')
-        .trim()
-        .isUUID(4)
-        .withMessage('Channel id is not valid'),    
+   param('id').trim().isUUID(4).withMessage('Channel id is not valid'),
 
-    handleValidationErrors, 
+   handleValidationErrors,
 ]
