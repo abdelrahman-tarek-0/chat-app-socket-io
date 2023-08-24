4759bca7cd3ad2f2adcbe1b66dd8727f3087dc5d
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-08-24 22:05:21 +0300

    [UPDATE] channel-users validator is done and tested

--- src/app/validators/channels-users.validators.js
+++ src/app/validators/channels-users.validators.js
@@ -14,64 +14,64 @@ exports.createGeneralInvite = [
 ]
 
 exports.createDirectInvite = [
-    param('channelId')
-        .trim()
-        .isUUID(4)
-        .withMessage('Channel ID is not valid')
-        .isLength({ max: 64 })
-        .withMessage('Channel ID must be less than 64 characters'),
+   param('channelId')
+      .trim()
+      .isUUID(4)
+      .withMessage('Channel ID is not valid')
+      .isLength({ max: 64 })
+      .withMessage('Channel ID must be less than 64 characters'),
 
-    param('targetName')
-        .trim()
-        .isLength({ min: 8, max: 30 })
-        .withMessage('Target name must be between 8 to 30 characters')
-        .matches(/^[A-Za-z][A-Za-z0-9_@\-.]{0,}$/)
-        .withMessage('Target name is not valid')
-        .toLowerCase(),
+   param('targetName')
+      .trim()
+      .isLength({ min: 8, max: 30 })
+      .withMessage('Target name must be between 8 to 30 characters')
+      .matches(/^[A-Za-z][A-Za-z0-9_@\-.]{0,}$/)
+      .withMessage('Target name is not valid')
+      .toLowerCase(),
 
-    handleValidationErrors,
-    strict,
+   handleValidationErrors,
+   strict,
 ]
 
 exports.joinChannel = [
-    param('channelId')
-       .trim()
-       .isUUID(4)
-       .withMessage('Channel ID is not valid')
-       .isLength({ max: 64 })
-       .withMessage('Channel ID must be less than 64 characters'),
- 
-    handleValidationErrors,
-    strict,
- ]
+   param('channelId')
+      .trim()
+      .isUUID(4)
+      .withMessage('Channel ID is not valid')
+      .isLength({ max: 64 })
+      .withMessage('Channel ID must be less than 64 characters'),
 
- exports.leaveChannel = [
-    param('channelId')
-       .trim()
-       .isUUID(4)
-       .withMessage('Channel ID is not valid')
-       .isLength({ max: 64 })
-       .withMessage('Channel ID must be less than 64 characters'),
- 
-    handleValidationErrors,
-    strict,
- ]
+   handleValidationErrors,
+   strict,
+]
 
-    exports.kickUser = [
-        param('channelId')
-        .trim()
-        .isUUID(4)
-        .withMessage('Channel ID is not valid')
-        .isLength({ max: 64 })
-        .withMessage('Channel ID must be less than 64 characters'),
+exports.leaveChannel = [
+   param('channelId')
+      .trim()
+      .isUUID(4)
+      .withMessage('Channel ID is not valid')
+      .isLength({ max: 64 })
+      .withMessage('Channel ID must be less than 64 characters'),
 
-        param('userId')
-        .trim()
-        .isUUID(4)
-        .withMessage('User ID is not valid')
-        .isLength({ max: 64 })
-        .withMessage('User ID must be less than 64 characters'),
+   handleValidationErrors,
+   strict,
+]
 
-        handleValidationErrors,
-        strict,
-    ]
+exports.kickUser = [
+   param('channelId')
+      .trim()
+      .isUUID(4)
+      .withMessage('Channel ID is not valid')
+      .isLength({ max: 64 })
+      .withMessage('Channel ID must be less than 64 characters'),
+
+   param('userId')
+      .trim()
+      .isUUID(4)
+      .withMessage('User ID is not valid')
+      .isLength({ max: 64 })
+      .withMessage('User ID must be less than 64 characters'),
+
+   handleValidationErrors,
+   strict,
+]
