87b0c7ac5b73fe24df312ec891c9257af4efde15
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-09-04 17:19:28 +0300

    [UPDATE] trying to make the message table with discord standers

---
+++ src/config/database/tables/message.table.js
@@ -0,0 +1,36 @@
+exports.messages = (knex) => (table) => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid())
+    table.increments('raw_id').unique().unsigned().notNullable()
+
+
+    table
+        .uuid('sender_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .notNullable()
+
+    table
+        .uuid('channel_id')
+        .references('id')
+        .inTable('channels')
+        .onDelete('CASCADE')
+        .notNullable()
+
+    table
+        .string('content')
+        .notNullable()
+
+    table
+        .boolean('is_active')
+        .defaultTo(true)
+        
+    table
+        .uuid('reply_to')
+        .references('id')
+        .inTable('messages')
+        .onDelete('CASCADE')
+        .notNullable()
+
+    table.timestamps(true, true)
+};
