a183fa066cd92954275f4fd006587029f23fc2b0
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-09-05 22:16:38 +0300

    [UPDATE] created bonds_messages table

--- src/config/database/tables/message.table.js
+++ src/config/database/tables/message.table.js
@@ -27,9 +27,44 @@ exports.channels_messages = (knex) => (table) => {
     table
         .uuid('reply_to')
         .references('id')
-        .inTable('messages')
+        .inTable('channels_messages')
+        .onDelete('CASCADE')
+
+    table.timestamps(true, true)
+};
+
+exports.bonds_messages = (knex) => (table) => {
+    table.uuid('id').primary().defaultTo(knex.fn.uuid())
+    table.increments('raw_id').unique().unsigned().notNullable()
+
+    table
+        .uuid('sender_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .notNullable()
+
+    table
+        .uuid('bond_id')
+        .references('id')
+        .inTable('bonds')
+        .onDelete('CASCADE')
+        .notNullable()
+
+    table
+        .string('content')
+        .notNullable()
+
+    table
+        .boolean('is_active')
+        .defaultTo(true)
+        
+    table
+        .uuid('reply_to')
+        .references('id')
+        .inTable('bonds_messages')
         .onDelete('CASCADE')
         .notNullable()
 
     table.timestamps(true, true)
-};
+}
