10c8677eeb4891cdd528ed90e73f63a28b707cbb
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-09-05 22:25:33 +0300

    [UPDATE] i made the message stand alone from ether the bond logic anc channels logic

--- src/config/database/tables/message.table.js
+++ src/config/database/tables/message.table.js
@@ -1,70 +1,39 @@
-exports.channels_messages = (knex) => (table) => {
-    table.uuid('id').primary().defaultTo(knex.fn.uuid())
-    table.increments('raw_id').unique().unsigned().notNullable()
+exports.messages = (knex) => (table) => {
+   table.uuid('id').primary().defaultTo(knex.fn.uuid())
+   table.increments('raw_id').unique().unsigned().notNullable()
 
-    table
-        .uuid('sender_id')
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .notNullable()
+   table
+      .uuid('sender_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .notNullable()
 
-    table
-        .uuid('channel_id')
-        .references('id')
-        .inTable('channels')
-        .onDelete('CASCADE')
-        .notNullable()
+   table.string('content').notNullable()
 
-    table
-        .string('content')
-        .notNullable()
+   table.boolean('is_active').defaultTo(true)
 
-    table
-        .boolean('is_active')
-        .defaultTo(true)
-        
-    table
-        .uuid('reply_to')
-        .references('id')
-        .inTable('channels_messages')
-        .onDelete('CASCADE')
+   table
+      .uuid('reply_to')
+      .references('id')
+      .inTable('channels_messages')
+      .onDelete('CASCADE')
+}
 
-    table.timestamps(true, true)
-};
+exports.attachments = (knex) => (table) => {
+   table.uuid('id').primary().defaultTo(knex.fn.uuid())
+   table.increments('raw_id').unique().unsigned().notNullable()
 
-exports.bonds_messages = (knex) => (table) => {
-    table.uuid('id').primary().defaultTo(knex.fn.uuid())
-    table.increments('raw_id').unique().unsigned().notNullable()
+   table
+      .uuid('message_id')
+      .references('id')
+      .inTable('messages')
+      .onDelete('CASCADE')
+      .notNullable()
 
-    table
-        .uuid('sender_id')
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .notNullable()
+   table.string('type').notNullable()
 
-    table
-        .uuid('bond_id')
-        .references('id')
-        .inTable('bonds')
-        .onDelete('CASCADE')
-        .notNullable()
+   table.string('url').notNullable()
 
-    table
-        .string('content')
-        .notNullable()
-
-    table
-        .boolean('is_active')
-        .defaultTo(true)
-        
-    table
-        .uuid('reply_to')
-        .references('id')
-        .inTable('bonds_messages')
-        .onDelete('CASCADE')
-        .notNullable()
-
-    table.timestamps(true, true)
+   table.timestamps(true, true)
 }
