e63c25e6bcf0405353c8b97d2f6cb75722870a11
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-09-07 20:48:57 +0300

    [UPDATE] the router and the controller for getMessages is done

--- src/app/controllers/messages.controller.js
+++ src/app/controllers/messages.controller.js
@@ -4,3 +4,14 @@ const resBuilder = require('../utils/responseBuilder')
 const ErrorBuilder = require('../utils/ErrorBuilder')
 const catchAsync = require('../utils/catchAsync')
 
+exports.getMessages = catchAsync((mode) => async (req, res, next) => {
+    const id = mode === 'channel' ? req?.params?.channelId : req?.params?.bondId
+
+    const messages = await Message.getMessages({
+        id,
+        mode,
+    })
+
+    
+    return resBuilder(res, 200, 'messages retrieved successfully', messages)       
+})
--- src/app/routes/APIs/messages.routes.js
+++ src/app/routes/APIs/messages.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 
-const {} = require('../../controllers/messages.controller')
+const { getMessages } = require('../../controllers/messages.controller')
 
 const { loggedIn } = require('../../middlewares/auth.middleware')
 
 const router = Router()
+
+router.get('/channels/:channelId', loggedIn(), )
 
 module.exports = router
