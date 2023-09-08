0b15e6c5872505366ca547387de315e09e7bf63c
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2023-09-08 03:05:16 +0300

    [UPDATE] channged get messages logic to implement the limit without losing any preformance

--- src/app/models/messages.model.js
+++ src/app/models/messages.model.js
@@ -3,68 +3,79 @@ const ErrorBuilder = require('../utils/ErrorBuilder')
 
 class Message {
    static async getBondMessages({ bondId, userId }) {
+      const data = await db.raw(
+         `
+          ${db('bonds as bond')
+             .select('bond.id as bond_id', 'bond.user1_id', 'bond.user2_id')
+             .where('bond.id', '=', bondId)
+             .andWhere(function () {
+                this.where('user1_id', userId)
+                this.orWhere('user2_id', userId)
+             })
+             .andWhere('status', '=', 'active')
+             .toString()};
 
-      let data = db('bonds as bond')
-         .select('bond.id as bond_id', 'bond.user1_id', 'bond.user2_id')
-         .where('bond.id', '=', bondId)
-         .andWhere(function () {
-            this.where('user1_id', userId)
-            this.orWhere('user2_id', userId)
-         })
-         .andWhere('status', '=', 'active')
-    
-
-      data = data
-         .select(
-            db.raw(
-               `json_agg(
-                  (json_build_object(
+            ${db('bonds_messages as bm')
+            .select(
+               'message.id as id',
+               db.raw(`
+                  CASE
+                     WHEN message.sender_id = ? THEN 'sent'
+                     ELSE 'received'
+                  END as type
+               `, [userId]),
+               db.raw(`
+                  CASE
+                     WHEN message.is_active = true THEN message.content
+                     ELSE null
+                  END as content
+               `),
+               db.raw(`
+                  json_build_object(
                      'id', message.id,
-                     'content', 
-                        (CASE
-                              WHEN message.is_active = true THEN message.content
-                              ELSE NULL
-                        END)
-                     ,
-                     'sender_id', message.sender_id,
-                     'sender_username', sender.username,
-                     'sender_image', sender.image_url,
-                     'reply_to', json_build_object(
-                           'id', reply.id,
-                           'content', (
-                                 CASE
-                                    WHEN reply.is_active = true THEN reply.content
-                                    ELSE NULL
-                                 END
-                           )
-                        ),
-                     'sent_at', bonds_messages.created_at,
-                     'message_created_at', message.created_at
-                  )) order by message.created_at ASC
-                  
-               )  as "messages"
-           `
+                     'content',(
+                        CASE
+                           WHEN reply.is_active = true THEN reply.content
+                           ELSE NULL
+                        END
+                     )
+                  ) as reply_to
+               `),
+               'message.sender_id as sender_id',
+               'sender.username as sender_username',
+               'sender.display_name as sender_display_name',
+               'sender.image_url as sender_image_url',
+               'bm.created_at as sent_at',
+               'message.created_at as created_at',
+               'message.updated_at as updated_at',
             )
+            .leftJoin('messages as message', 'message.id', 'bm.message_id')
+            .leftJoin('messages as reply', 'reply.id', 'message.reply_to')
+            .leftJoin('users as sender', function () {
+               this.on('sender.id', '=', 'message.sender_id')
+               this.on('sender.is_active', '=', db.raw('?', ['true']))
+            })
+            .where('bm.bond_id', '=', bondId)
+            .orderBy('bm.created_at', 'desc')
+            .limit(50)
+            .toString()
+            }  
+         `
+      )
+
+      const bond = data?.[0]?.rows?.[0]
+      const messages = data?.[1]?.rows
+
+      if (!bond?.bond_id) {
+         throw new ErrorBuilder(
+            'Bond not found or inactive',
+            404,
+            'BOND_NOT_FOUND'
          )
-         .leftJoin('bonds_messages', 'bond_id', 'bond.id')
-         .leftJoin('messages as message', 'message.id', 'bonds_messages.message_id')
-         .leftJoin('messages as reply', 'reply.id', 'message.reply_to')
-         .leftJoin('users as sender', function () {
-            this.on('sender.id', '=', 'message.sender_id')
-            this.on('sender.is_active', '=', db.raw('?', ['true']))
-         })
-         
+      }
 
-      data = await data.groupBy('bond.id', 'sender.id')
-
-      if (data.length === 0) {
-         throw new ErrorBuilder('Bond not found', 404, 'BOND_NOT_FOUND')
-      }  
-
-      return data
+      return messages
    }
 }
-
-
 
 module.exports = Message
